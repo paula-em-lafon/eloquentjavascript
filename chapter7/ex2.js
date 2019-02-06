42645d12c785d52509b4e06469f7038651a40416
paula-em-lafon <paula.em.lafon@gmail.com>
2019-02-05 21:03:32 -0600

    ex 7.2 solved

--- chapter7/ex2.js
+++ chapter7/ex2.js
@@ -1,16 +1,22 @@
+//new robot function works better
+
 function yourRobot({place, parcels}, route) {
-  var routeHolder = null;
-  for plac in parcel{
-    if (route.length == 0) {
-      let parcel = parcels[0];
+  if (route.length == 0) {
+    placeholder = roads;
+    for(let i = 0; i< parcels.length;i++){
+      let parcel = parcels[i];
       if (parcel.place != place) {
         route = findRoute(roadGraph, place, parcel.place);
       } else {
         route = findRoute(roadGraph, place, parcel.address);
       }
+      if(route.length < placeholder.length){
+        placeholder = route
+      }
     }
+    route = placeholder;
   }
   return {direction: route[0], memory: route.slice(1)};
 }
 
-runRobotAnimation(VillageState.random(), yourRobot, memory);
+compareRobots(goalOrientedRobot, [], yourRobot, []);
